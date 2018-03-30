889d01071ca52e22c4aa211897afd36087ef61cc
Glitch (minesweeper-game) <none>
2018-03-30 03:22:07 +0000

    🛳🎽 Checkpoint ./app/components/Board.jsx:217543/1063

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -2,13 +2,24 @@ const React = require('react');
 
 const Square = require('./Square');
 
-const Board = function() {
+
+
+const Board = function(props) {
+  
+  const squares = props.board.map((row) => {
+    <li>
+      <ul>
+        !{row}
+      </ul>                                   
+    </li>
+  });
+  
   return (
     <div>
       <h3>Board</h3>
-      <Square n={numb()} />
-      <Square n={numb()} />
-      <Square n={numb()} />
+      <h4>{typeof props.board} - {props.board.length}</h4>
+      <p>{props.board}</p>
+      <ul>{squares}</ul>
     </div>
   );
 }
