91bbc7f8644170d45c9cb9777a83c4164ced774a
Glitch (beachcomber) <none>
2018-03-30 23:08:38 +0000

    🎗💽 Checkpoint ./app/components/Animations.jsx:217543/547 ./public/style.css:217543/2

--- app/components/Animations.jsx
+++ app/components/Animations.jsx
@@ -3,11 +3,20 @@ const React = require('react');
 
 const Animations = function(props) {
   
+  getIcon(i, type) {
+    var winIcons = ["👒","🎩","🎓","🥇","👑","🕶","🐣","☘️","🌈","⚡️","🎈","🎏","🎉","🎊","📎","💯","🐬"];
+    var loseIcons = ["🍃","🥀","☄️","🌩","💦","🎭","🔮","❌","🌀♣️🏁];
+    
+    
+  }
+  
+  
   function getItems() {
     var newAnimationItems = [];
     if (props.win || props.lose) {
+      var type = props.win ? "win" : "lose";
       for (var i = 0; i < 600; i++) {
-        newAnimationItems.push({icon: "😇", id: newAnimationItems.length});
+        newAnimationItems.push({icon: getIcon(i, type), id: newAnimationItems.length});
       }
     }
     return newAnimationItems;
@@ -25,15 +34,7 @@ const Animations = function(props) {
                                 
   function place() {
     var left = Math.floor(Math.random() * 120) - 10;
-    var top = Math.floor((Math.random() * Math.random()) * -2800) - 10;
-    /*
-    var top = 0
-    for (var i = 0; i < 2; i++) {
-      top += Math.random() * (2800/2);
-    }
-    top = Math.floor(top * - 1) - 10;
-    console.log(top);
-    */
+    var top = Math.floor((((Math.random() * Math.random() * Math.random())  +  Math.random())/2) * -2800) - 10;
     return {left: left + "vw", top: top + "vh"};
   }
   
