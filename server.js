b1a020340a2e939195a768044e30ee5167f0192f
Glitch (pay-to-win) <none>
2020-11-12 16:40:42 +0000

    🐿🍱 Checkpoint ./server.js:24872488/1061

--- server.js
+++ server.js
@@ -65,6 +65,29 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
+// Match the raw body to content type application/json
+app.post('/webhook', (request, response) => {
+  const event = req.body;
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log('PaymentIntent was successful!');
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.json({received: true});
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
